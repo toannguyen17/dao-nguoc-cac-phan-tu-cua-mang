62dafe2dff2f9119bb36173282a584b9a3890fc3
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-04-28 15:27:57 +0700

    add project

---
+++ src/app/App.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		int size;
+		int[] array;
+		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.print("Enter a size: ");
+			size = scanner.nextInt();
+			if (size > 20)
+				System.out.println("Size does not exceed 20");
+		} while (size > 20);
+
+		// 
+		array = new int[size];
+		int i = 0;
+		while (i < array.length) {
+			System.out.print("Enter element " + (i + 1) + ": ");
+			array[i] = scanner.nextInt();
+			i++;
+		}
+
+		// print
+		System.out.print("Elements in array: ");
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + "\t");
+		}
+
+		// Đảo ngược
+		for (int j = 0; j < array.length/2; j++) {
+			int temp = array[j];
+			array[j] = array[size - 1 - j];
+			array[size - 1 - j] = temp;
+		}
+
+		// print
+		System.out.println("Reverse array: ");
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + "\t");
+		}
+	}
+}
